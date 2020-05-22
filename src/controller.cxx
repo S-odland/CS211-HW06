bb123ce1dd5641ca389f5158b62f5eaabb717f47
S-odland <49540258+S-odland@users.noreply.github.com>
2020-05-21 21:01:20 -0400

    Initial download of skeleton files

---
+++ src/controller.cxx
@@ -0,0 +1,25 @@
+#include "controller.hxx"
+
+Controller::Controller(int size)
+        : Controller(size, size)
+{ }
+
+Controller::Controller(int width, int height)
+        : model_(width, height)
+        , view_(model_)
+{ }
+
+void Controller::draw(ge211::Sprite_set& sprites)
+{
+    view_.draw(sprites);
+}
+
+ge211::Dimensions Controller::initial_window_dimensions() const
+{
+    return view_.initial_window_dimensions();
+}
+
+std::string Controller::initial_window_title() const
+{
+    return view_.initial_window_title();
+}
