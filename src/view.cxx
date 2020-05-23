f5750a7300ec375572576f0eefe52c7eaa22575f
S-odland <49540258+S-odland@users.noreply.github.com>
2020-05-23 16:47:33 -0400

    View.cxx/hxx updates

--- src/view.cxx
+++ src/view.cxx
@@ -1,4 +1,5 @@
 #include "view.hxx"
+#include <cmath>
 
 // Convenient type aliases:
 using Dimensions = ge211::Dimensions;
@@ -8,15 +9,39 @@ using Sprite_set = ge211::Sprite_set;
 
 // You can change this or even determine it some other way:
 static int const grid_size = 36;
+static int const sprite_radius = 6;
+
+static ge211::Color const
+        dark_sprite_color = ge211::Color::black(),
+        light_sprite_color = ge211::Color::white(),
+        dark_hover = dark_sprite_color.lighten(0.5),
+        light_hover = dark_sprite_color.lighten(0.5),
+        loser_color = ge211::Color {128,128,128},
+        grid_color = ge211::Color {0, 255, 0};
+
 
 View::View(Model const& model)
         : model_(model)
-        // You may want to add sprite initialization here
+        , dark_sprite_(sprite_radius, dark_sprite_color)
+        , light_sprite_(sprite_radius, light_sprite_color)
+        , dark_hover_sprite_(sprite_radius, dark_hover)
+        , light_hover_sprite_(sprite_radius, light_hover)
+        , loser_sprite_(sprite_radius, loser_color)
+        , grid_sprite_({grid_size - 2,grid_size - 2},grid_color)
 { }
 
-void View::draw(Sprite_set& set)
+void View::draw(Sprite_set& set, Position pos, bool b)
 {
-    // TODO, PROBABLY
+    for ( ge211::Position pos : model_.board() )
+    {
+        set.add_sprite(grid_sprite_,screen_to_board(pos),0);
+    }
+
+    if (b){
+        add_player_(set,model_.turn(),pos,1);
+    } else {
+        hover_player_(set,model_.turn(),pos);
+    }
 }
 
 Dimensions View::initial_window_dimensions() const
@@ -29,4 +54,74 @@ std::string View::initial_window_title() const
 {
     // You can change this if you want:
     return "Reversi";
+}
+
+void View::add_player_(ge211::Sprite_set &set,
+                        Player p,
+                        ge211::Position pos,
+                        int z) const
+{
+    if(model_.is_game_over()) {
+        if (model_.winner() == Player::dark) {
+            for (ge211::Position pos : model_.board()) {
+                if (model_.operator[](pos) ==
+                    Player::light) {
+                    set.add_sprite(loser_sprite_, board_to_screen(pos), z);
+                }
+            }
+        } else if (model_.winner() == Player::light) {
+            for (ge211::Position pos : model_.board()) {
+                if (model_.operator[](pos) == Player::dark) {
+                    set.add_sprite(loser_sprite_, board_to_screen(pos), z);
+                }
+            }
+        }
+    } else {
+        if (p == Player::dark) {
+            set.add_sprite(dark_sprite_, pos, z);
+        } else if (p == Player::light) {
+            set.add_sprite(light_sprite_, pos, z);
+        }
+    }
+}
+
+void View::hover_player_(ge211::Sprite_set &set,
+                       Player p,
+                       ge211::Position pos) const
+{
+    if (p == Player::dark){
+        set.add_sprite(dark_hover_sprite_, pos, 1);
+        Position_set pos_set = model_.find_move(screen_to_board(pos))->second;
+
+        for (Position poss : pos_set)
+        {
+            set.add_sprite(dark_sprite_, board_to_screen(poss), 2);
+        }
+
+    } else if (p == Player::light){
+        set.add_sprite(light_hover_sprite_, pos, 1);
+        Position_set pos_set = model_.find_move(screen_to_board(pos))->second;
+
+        for (Position poss : pos_set)
+        {
+            set.add_sprite(dark_sprite_, board_to_screen(poss), 2);
+        }
+
+    }
+}
+
+Position View::screen_to_board(Position pos) const
+{
+    pos.x = pos.x / grid_size;
+    pos.y = pos.y / grid_size;
+
+    return pos;
+}
+
+Position View::board_to_screen(Position pos) const
+{
+    pos.x = pos.x * grid_size;
+    pos.y = pos.y * grid_size;
+
+    return pos;
 }
--- src/view.hxx
+++ src/view.hxx
@@ -12,7 +12,10 @@ public:
     // You will probably want to add arguments here so that the
     // controller can communicate UI state (such as a mouse or
     // cursor position):
-    void draw(ge211::Sprite_set& set);
+    // pos will be determined by the mouse position in controller and b will
+    // signify whether or not the mouse has been clicked or not
+
+    void draw(ge211::Sprite_set& set, ge211::Position pos, bool b);
 
     ge211::Dimensions initial_window_dimensions() const;
 
@@ -21,6 +24,20 @@ public:
 private:
     Model const& model_;
 
-    // TODO: Add any private members you need, such as helper functions
-    // or sprites.
+    ge211::Circle_sprite dark_sprite_;
+    ge211::Circle_sprite light_sprite_;
+    ge211::Circle_sprite dark_hover_sprite_;
+    ge211::Circle_sprite light_hover_sprite_;
+    ge211::Circle_sprite loser_sprite_;
+    ge211::Rectangle_sprite grid_sprite_;
+
+    ge211::Position screen_to_board(ge211::Position pos) const;
+    ge211::Position board_to_screen(ge211::Position pos) const;
+    void add_player_(ge211::Sprite_set& set,
+                    Player p,
+                    ge211::Position pos,
+                    int z) const;
+    void hover_player_(ge211::Sprite_set& set,
+                     Player p,
+                     ge211::Position pos) const;
 };
